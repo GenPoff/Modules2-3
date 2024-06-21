bfa7773209179537b27ca233ea86d5905b1dbedb
GenPoff <mylene.ua@mail.ru>
2024-06-21 20:24:48 +0300

    Fixed task

--- module2_5.py
+++ module2_5.py
@@ -1,12 +1,13 @@
-def get_matrix():
-    n = 4
-    m = 5
-    value = 28
+def get_matrix(n, m, value):
     matrix = []
     for stroka in range(n):
         matrix.append(list())
         for stolb in list(range(m)):
             matrix[-1].append(value)
     return matrix
-matrix = get_matrix()
+matrix = get_matrix(4, 5, 28)
+matrix2 = get_matrix(2, 3, 7)
+matrix3 = get_matrix(5, 2, 15)
 print(matrix)
+print(matrix2)
+print(matrix3)
